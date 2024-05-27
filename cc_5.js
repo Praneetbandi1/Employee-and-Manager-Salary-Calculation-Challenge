bf241f139d53e553c73da0e769c038171eee9d37
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-05-27 19:49:32 -0400

    calculated and returned the annual salary

--- cc_5.js
+++ cc_5.js
@@ -8,6 +8,13 @@ class Employee {
         console.log(`Name of Employee: ${this.empName}`);
         console.log(`Salary of Employee: ${this.empSalary}`);
     }
+    //calculating the Employee Annual Salary
+    calculateEmpAnnualSalary() {
+        let annualSalary=this.empSalary * 12;
+        console.log(`Employee Annual Salary: $${annualSalary}`);
+        //returning the annual Salary of employee
+        return annualSalary;
+    }
 
 }
 
