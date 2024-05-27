ecae2c9564262ce0082c8dfa4aa83fe177e4d20b
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-05-27 19:38:23 -0400

    added employee class

---
+++ cc_5.js
@@ -0,0 +1,18 @@
+//U46368100
+// implementing the employee class
+class Employee {
+    // employee constructor
+    constructor(name, salary) {
+        this.empName = name;
+        this.empSalary = salary;
+        console.log(`Name of Employee: ${this.empName}`);
+        console.log(`Salary of Employee: ${this.empSalary}`);
+    }
+
+}
+
+
+
+
+
+
